7d9b6fab837ae062aecf50fb61e6429e3c72353a
Trevor Winn <trevor@rdiglobalhospitality.com>
2022-07-15 16:47:48 -0400

    It now jankly saves what you have clicked based on brand

--- public/javascript/AddItem.js
+++ public/javascript/AddItem.js
@@ -16,6 +16,7 @@ export class Add_Item{
         this.SelectedItemType; //Stores the Itemtype we chose
         this.EnteredSKU;
         this.BrandContainer = []; //Container for all of the itemdata
+        this.FinalSelected = [];
         this.Overlay_Content = document.getElementById("OVC");
         this.Overlay_Title = document.getElementById("OVT");
         this.SKU_Brand_Section = document.getElementById("SKUBRAND");
@@ -43,7 +44,9 @@ export class Add_Item{
         RemoveChildNodes(this.Overlay_Content);
         RemoveChildNodes(this.SKU_Brand_Section);
         RemoveChildNodes(this.Options_Section);
+        this.BrandContainer = [];
         this.ParseItemData();
+        this.StateHandler();
     }
 
     StateHandler(){
@@ -60,7 +63,7 @@ export class Add_Item{
     CreateSelection(Ele, Items){
         for(let  i = 0; i < Items.length; i++){
             Ele.appendChild(this.EleBuilder.Button({Button_Tag:false, A_Tag:true, _class:"InfoItem", 
-            TextContent:Items[i].ItemType, Data:{"DataName":"data-ItemType", "DataValue":Items[i].ItemType}}, 
+            TextContent:Items[i].ItemType.replace("_", " "), Data:{"DataName":"data-ItemType", "DataValue":Items[i].ItemType}}, 
             {Callback:function(){
                 if(this.AddProductObj.CurrentState == this.AddProductObj.States.ItemType){
                     this.AddProductObj.ItemType = Items[i].ItemType;
@@ -100,10 +103,20 @@ export class Add_Item{
      */
     AddOptions(SelectedBrand){
         RemoveChildNodes(this.Options_Section);
-        this.BrandContainer = [];
-        for(let n = 0; n < this.Brands.length; n++){
-            this.BrandContainer.push(new Brand(this.Brands[n]));
-            this.BrandContainer[n].AddItemType(new ItemType(this.AddProductObj.ItemType, this.BrandContainer[n]));
+        //this.BrandContainer = [];
+        var NewOptions;
+        if(this.BrandContainer.length > 0){
+            NewOptions = false;
+            this.ParseItemData();
+        }else{
+            NewOptions = true;
+        }
+        console.log("NewOption: " + NewOptions);
+        if(NewOptions == true){
+            for(let n = 0; n < this.Brands.length; n++){
+                this.BrandContainer.push(new Brand(this.Brands[n]));
+                this.BrandContainer[n].AddItemType(new ItemType(this.AddProductObj.ItemType, this.BrandContainer[n]));
+            }
         }
 
         var ColumnIndex = 1;
@@ -117,24 +130,31 @@ export class Add_Item{
         for(let i = 0; i < this.ItemDataContainer.length; i++){
             if(this.ItemDataContainer[i].ItemType == this.AddProductObj.ItemType){
                 for(let n = 0; n < this.BrandContainer.length; n++){
-                    
-                    for(let j = 0; j < this.ItemDataContainer[i].ItemOptions.length; j++){
-                        let _option = new Option(this.ItemDataContainer[i].ItemOptions[j], this.BrandContainer[n].Type);
-                        this.BrandContainer[n].Type.Options.push(_option);
-                        if(this.BrandContainer[n].BrandName == SelectedBrand.replace("_", "")){
-
+                    var AlreadyHasOption = this.BrandContainer[n].Type.Options.length == 0 ? false : true;
+                    if(this.BrandContainer[n].BrandName == SelectedBrand.replace("_", "")){
+                        for(let j = 0; j < this.ItemDataContainer[i].ItemOptions.length; j++){
+                            let _option = new Option(this.ItemDataContainer[i].ItemOptions[j], this.BrandContainer[n].Type);
+                            if(!AlreadyHasOption){
+                                this.BrandContainer[n].Type.Options.push(_option);
+                            }
                             for(let k = 0; k < this.ItemDataContainer[i].ItemOptionsValues[j][this.ItemDataContainer[i].ItemOptions[j]].length; k++){
                                 //Add Each Value to the Option Div here
+                                //console.log(Object.keys(this.ItemDataContainer[i].ItemOptionsValues[j]));
                                 let _value = new Value(this.ItemDataContainer[i].ItemOptionsValues[j][this.ItemDataContainer[i].ItemOptions[j]][k], _option);
-                                this.BrandContainer[n].Type.Options[j].Values.push(_value);
+                                if(!AlreadyHasOption){
+                                    this.BrandContainer[n].Type.Options[j].Values.push(_value);
+                                }else{
+                                    _option.Values.push(this.BrandContainer[n].Type.Options[j].Values[k]);
+                                }
                             }  
-
+                            //console.log(RowIndex, ColumnIndex);
                             if(ColumnIndex == 1 && RowIndex == 1){
                                 this.BuildColumn(SheetContainer, ColumnId, ColumnIndex);
-                                this.BuildRow(ColumnId, ColumnIndex, RowId, RowIndex, _option);
+                                console.log(_option);
+                                this.BuildRow(ColumnId, ColumnIndex, RowId, RowIndex, this.BrandContainer[n], _option);
                                 RowIndex++;
                             }else{
-                                this.BuildRow(ColumnId, ColumnIndex, RowId, RowIndex, _option);
+                                this.BuildRow(ColumnId, ColumnIndex, RowId, RowIndex, this.BrandContainer[n], _option);
                                 RowIndex++;
                                 if(RowIndex % 3 == 1 && j < this.ItemDataContainer[i].ItemOptions.length-1){
                                     ColumnIndex++;
@@ -160,31 +180,76 @@ export class Add_Item{
      * @param {number} RowIndex 
      * @param {Option} _option 
      */
-    BuildRow(ColumnId, ColumnIndex, RowId, RowIndex, _option){
+    BuildRow(ColumnId, ColumnIndex, RowId, RowIndex, BrandContainer, _option){
+        //console.log("Adding");
         var Inner = "InnerRow";
         document.getElementById(ColumnId+ColumnIndex).appendChild(this.EleBuilder.Div({_id:RowId+RowIndex, _class:"Row"}));
         document.getElementById(RowId+RowIndex).appendChild(this.EleBuilder.Div({_id:RowId+RowIndex+Inner, _class:"InnerRow"}));
         document.getElementById(RowId+RowIndex).prepend(this.EleBuilder.Label({TextContent:_option.OptionName, colorChange:{A:true, Color:"white"}}));
+        //console.log("Length: " + _option.Values.length);
         for(let i = 0; i < _option.Values.length; i++){
             if(_option.GetUIType() == "CheckBox"){
-                document.getElementById(RowId+RowIndex+Inner).appendChild(this.EleBuilder.Label({TextContent:_option.Values[i].ValueName, colorChange:{A:true, Color:"white"}}));
-                document.getElementById(RowId+RowIndex+Inner).appendChild(this.EleBuilder.Input({type:"checkbox", value:_option.Values[i].ValueName, name:_option.OptionName}));
+                var Name;
+                if(_option.Values[i].ValueName === ""){
+                    Name = "TextBox";
+                }else{
+                    Name = _option.Values[i].ValueName;
+                }
+                document.getElementById(RowId+RowIndex+Inner).appendChild(this.EleBuilder.Label({TextContent:Name, colorChange:{A:true, Color:"white"}}));
+                document.getElementById(RowId+RowIndex+Inner).appendChild(this.EleBuilder.Input({type:"checkbox", value:_option.Values[i].ValueName, checked:_option.Values[i].Value, name:_option.OptionName}, {CallbacK:this.HandleCheckBox.bind(this), CBParam:[_option, BrandContainer, _option.Values[i]]}));
             }
             if(_option.GetUIType() == "TextBox"){
-                document.getElementById(RowId+RowIndex+Inner).appendChild(this.EleBuilder.TextBox({col:20, row:2, MaxLength:30}));
+                document.getElementById(RowId+RowIndex+Inner).appendChild(this.EleBuilder.TextBox({col:20, row:2, MaxLength:30}, {Callback:this.Test.bind(this), CBParam:[_option, BrandContainer]}));
             }
             if(_option.GetUIType() == "Radial"){
                 var f = 'Field';
+                var Check_True = _option.Values[i].Value === "true" ? true : false;
+                var Check_False = _option.Values[i].Value === "false" ? true : false;
                 document.getElementById(RowId+RowIndex+Inner).appendChild(this.EleBuilder.FieldSet({_id:RowId+RowIndex+Inner+f}));
-
                 document.getElementById(RowId+RowIndex+Inner+f).appendChild(this.EleBuilder.Label({TextContent:"True", colorChange:{A:true, Color:"white"}}));
-                document.getElementById(RowId+RowIndex+Inner+f).appendChild(this.EleBuilder.Input({type:"radio", value:true, name:_option.OptionName}));
+                document.getElementById(RowId+RowIndex+Inner+f).appendChild(this.EleBuilder.Input({type:"radio", value:"true", name:_option.OptionName, checked:Check_True}, {CallbacK:this.HandleRadioButton.bind(this), CBParam:[_option, BrandContainer], Event:'change'}));
                 document.getElementById(RowId+RowIndex+Inner+f).appendChild(this.EleBuilder.Label({TextContent:"False", colorChange:{A:true, Color:"white"}}));
-                document.getElementById(RowId+RowIndex+Inner+f).appendChild(this.EleBuilder.Input({type:"radio", value:false, name:_option.OptionName}));
+                document.getElementById(RowId+RowIndex+Inner+f).appendChild(this.EleBuilder.Input({type:"radio", value:"false", name:_option.OptionName, checked:Check_False }, {CallbacK:this.HandleRadioButton.bind(this), CBParam:[_option, BrandContainer], Event:'change'}));
             }
         }
-
     }
+
+    HandleRadioButton(Params){
+        var _option = Params.Params[0];
+        var _brand = Params.Params[1];
+        var Event = Params.e;
+        console.log(_brand);
+        if(Event.target.checked && Event.target.value === "true"){
+            _option.Values[0].Value = "true";
+        }else if(Event.target.checked && Event.target.value === "false"){
+            _option.Values[0].Value = "false";
+        }
+    }
+    //TODO Should probably be more thoughtful about whether I send Value or Option
+    HandleCheckBox(Params){
+        var _option = Params.Params[0];
+        var _brand = Params.Params[1];
+        var Value = Params.Params[2];
+        var Event = Params.e;
+        console.log(Event.target.checked);
+        if(Event.target.checked === true){
+            Value.Value = true;
+        }else{
+            Value.Value = false;
+        }
+        console.log("CheckBox");
+        //_option.Values[0].UpdateValue(Event.target.value);
+    }
+
+
+
+    Test(Params){
+        console.log("Test");
+        console.log(Params.Params[0], Params.Params[1]);
+        Params.Params[0].Values[0].UpdateValue(Params.e.target.value);
+        console.log(Params.Params[0].Selected);
+    }
+
 
     ParseItemData(){
         this.ItemDataContainer = [];
@@ -196,7 +261,7 @@ export class Add_Item{
                 this.ItemDataContainer[i].ItemOptionsValues.push(this.AllData[i].Options[j]);
             }
         }
-        this.StateHandler();
+        //this.StateHandler();
     }
     
 
@@ -248,7 +313,7 @@ class ElementBuilder{
         ResizeAble = false, 
         col = 10, row =  10, MaxLength = 20,
         defaultText = "",
-        _class = ""  } = {}, {Callback = undefined, CBParam = [], Event="selectionchange",
+        _class = ""  } = {}, {Callback = undefined, CBParam = [], Event="input",
                                 Data={A:false, DataName:"", DataValue:""}} = {})
         {
         var t = document.createElement("textarea");
@@ -274,8 +339,9 @@ class ElementBuilder{
         if(Callback == undefined){
             return t;
         }
-        s.addEventListener(Event, () => {
-            Callback(CBParam);
+        t.addEventListener(Event, (e) => {
+            var Params = CBParam;
+            Callback({Params, e});
         });
         return t;
     }
@@ -367,7 +433,9 @@ class ElementBuilder{
     }
     Label({_class = "", TextContent="", _id = "", colorChange={A:false, Color:""}} = {}){
         var  l = document.createElement("label");
-        l.textContent = TextContent;
+        var Text = JSON.stringify(TextContent);
+        var newText = Text.replace(/_/g, " ");
+        l.textContent = newText.replace(/"/g, "");
         if(_class != ""){
             l.setAttribute("class", _class);
         }
@@ -379,7 +447,7 @@ class ElementBuilder{
         }
         return l;
     }
-    Input({_class="", _id="", type="", value="", name=""} = {}, {CallbacK = undefined, CBParam = [], Event="click"} = {}){
+    Input({_class="", _id="", type="", value="", name="", checked=false} = {}, {CallbacK = undefined, CBParam = [], Event="click"} = {}){
         var  i = document.createElement("input");
         if(_class != ""){
             i.setAttribute("class", _class);
@@ -396,11 +464,14 @@ class ElementBuilder{
         if(name != ""){
             i.setAttribute("name", name);
         }
+        i.checked = checked;
+
         if(CallbacK == undefined){
             return i;
         }
-        i.addEventListener(Event, () => {
-            CallbacK(CBParam);
+        i.addEventListener(Event, (e) => {
+            var Params = CBParam;
+            CallbacK({Params, e});
         });
 
         return i;
@@ -417,6 +488,7 @@ class ElementBuilder{
 class Brand{
     constructor(Name){
         this.BrandName = Name;
+        this.Selected = false;
         this.Type;
     }
     AddItemType(ItemTypeObject){
@@ -468,17 +540,29 @@ class Option{
         if(typeof this.Values[0].ValueName == "boolean"){
             return "Radial";
         }
-        if(this.Values[0].ValueName === ""){
-            return "TextBox";
-        }
+        //if(this.Values[0].ValueName === ""){
+        //    return "TextBox";
+        //}
         return "CheckBox";
     }
 }
 
 class Value{
+    /**
+     * 
+     * @param {string} Name 
+     * @param {Option} _Parent 
+     */
     constructor(Name, _Parent){
         this.ValueName = Name;
+        this.Value = undefined;
         this.Selected = false;
         this.Parent = _Parent;
     }
+    UpdateValue(val){
+        this.Value = val;
+        this.Selected = true;
+        this.Parent.IsThisOptionSelected();
+    }
+
 }
