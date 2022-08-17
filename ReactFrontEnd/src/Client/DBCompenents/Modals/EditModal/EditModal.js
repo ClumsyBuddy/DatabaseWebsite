9b310f40565e8cafe17b245b0b5b0f00c154c7a4
Trevor Winn <trevor@rdiglobalhospitality.com>
2022-08-17 13:44:17 -0400

    Edit modal UI finished

--- ReactFrontEnd/src/Client/DBCompenents/Modals/EditModal/EditModal.js
+++ ReactFrontEnd/src/Client/DBCompenents/Modals/EditModal/EditModal.js
@@ -25,80 +25,76 @@ const EditModal = ({...props}) => {
                 return res.json();
               })
               .then(data => {
-                console.log(data);
                 const AllOptions = data.Options;
-                let Options = {};
-                AllOptions.forEach(element => {
-                    Object.assign(Options, element);
-                });
-                setItemData(Options);
-                console.log(props.editProduct);
+                setItemData(AllOptions);
+                console.log(AllOptions, props.editProduct);
               });
         }
         
 
         if(!mounted.current){
             FetchItemData();
-            console.log(props.editProduct.key);
         }
         mounted.current = true;
     }, []);
 
     
-    // const RenderOptions = ItemData.map((item) => {
-
-    //     return (<div className='OList' style={{width:"50vw", height:"55vh", overflow:"scroll"}}>
-    //         <label>{props.editProduct.sku}</label>
-    //         <label>
-    //             {props.editProduct.itemtype}
-    //         </label>
-    //         <div style={{width:"100%", height:"auto", display:"flex", 
-    //             flexDirection:"row", justifyContent:"center", 
-    //             flexFlow:"wrap", overflow:"hidden"}}>
-    //         {
-    //             item.Options.length !== 0 ? item.Options.map((option) => 
-    //             {
-    //                 let optionName = Object.keys(option)[0];
-    //                 return (<div style={{width:"20%", borderStyle:"solid", borderColor:"black", margin:"1%", borderRadius:"10px", padding:"10px", 
-    //                         display:"block", fontSize:"20px"}}>
-    //                         <label>{Object.keys(option)[0].replace(/_/g, " ")}: </label>
-    //                         {
-    //                             option[optionName].map((element, i) => 
-    //                             {
-    //                                 if(element === "" || element === true || element === false){
-    //                                     return (<> {element === "" ? <label style={{textDecoration:"underline"}}>textbox</label> : <></>}
-    //                                     <input type={"checkbox"} className='ButtonHoverEffect ItemTypeButton' onChange={(e) => {let _new = selected; _new[optionName] = e.currentTarget.checked; setSelected(_new); console.log(selected); }}></input> </>
-    //                                     );
-    //                                 }
-    //                                 return ( <>
-    //                                     <label>{element}</label>
-    //                                     <input type={"checkbox"} className='ButtonHoverEffect ItemTypeButton' onChange={(e)=>
-    //                                     {
-    //                                         let _new = selected; 
-    //                                         if(e.currentTarget.checked === false){
-    //                                             let toReplace = element + ",";
-    //                                             console.log(toReplace);
-    //                                             _new[optionName] = _new[optionName].replace(toReplace, "");
-    //                                         }else{
-    //                                             console.log(element);
-    //                                             if(_new[optionName] === undefined){
-    //                                                 _new[optionName] = element + ",";
-    //                                             }else{
-    //                                                 _new[optionName] += element + ',';
-    //                                             }
-    //                                         }
-    //                                         console.log(_new);
-    //                                         setSelected(_new);
-    //                                     }}></input> 
-    //                                 </>);
-    //                             })
-    //                         }
-    //             </div>);
-    //         }) : <p>Item Has No Options</p> 
-    //     }</div></div>);
-    
-        
-    // })
+    const RenderOptions = () => {
+        return (
+            <div className='OList' style={{width:"50vw", height:"55vh", overflow:"scroll"}}>
+            <label>
+                {props.editProduct.itemtype}
+            </label>
+            <br></br>
+            <label>{props.editProduct.sku}</label>
+            <label>{props.editProduct.brand}</label>
+            
+            <div style={{width:"100%", height:"auto", display:"flex", 
+                flexDirection:"row", justifyContent:"center", flexFlow:"wrap", overflow:"hidden"}}>
+            {
+                ItemData.map((option) => 
+                {
+                    let optionName = Object.keys(option)[0];
+                    return (
+                        <div style={{width:"20%", borderStyle:"solid", borderColor:"black", margin:"1%", borderRadius:"10px", padding:"10px", 
+                                display:"block", fontSize:"20px"}}>
+                                <label>{Object.keys(option)[0].replace(/_/g, " ")}: </label>
+                                {
+                                    option[optionName].map((element, i) => 
+                                    {
+                                        if(element === "" || element === true || element === false){
+                                            return (<> {element === "" ? <label style={{textDecoration:"underline"}}>textbox</label> : <></>}
+                                            <input type={"checkbox"} className='ButtonHoverEffect ItemTypeButton' onChange={(e) => {let _new = selected; _new[optionName] = e.currentTarget.checked; setSelected(_new); console.log(selected); }}></input> </>
+                                            );
+                                        }
+                                        return <>
+                                            <label>{element}</label>
+                                            <input type={"checkbox"} defaultChecked={
+                                                props.editProduct[optionName] !== null ? !!props.editProduct[optionName].includes(element) : false
+                                            } className='ButtonHoverEffect ItemTypeButton' onChange={(e)=>
+                                            {
+                                                let _new = selected; 
+                                                if(e.currentTarget.checked === false){
+                                                    let toReplace = element + ",";
+                                                    _new[optionName] = _new[optionName].replace(toReplace, "");
+                                                }else{
+                                                    if(_new[optionName] === undefined){
+                                                        _new[optionName] = element + ",";
+                                                    }else{
+                                                        _new[optionName] += element + ',';
+                                                    }
+                                                }
+                                                setSelected(_new);
+                                            }}></input> 
+                                        </>;
+                                    })
+                                }
+                    </div>
+                    );
+            })
+        }</div></div>
+        );
+    }
 
    
 
@@ -107,9 +103,9 @@ const EditModal = ({...props}) => {
             console.log("Missing SKU or Brand");
             return false;
         }
-        socket.emit("add_Item", selected, (result) => {
-            console.log("Result: " + JSON.stringify(result));
-        });
+        // socket.emit("add_Item", selected, (result) => {
+        //     console.log("Result: " + JSON.stringify(result));
+        // });
     }
 
     return (
@@ -119,7 +115,7 @@ const EditModal = ({...props}) => {
                 <div id='Container' className='Container'>
                     <h1>Edit Modal</h1>
                     <div className='OptionsContainer' style={{overflow:"scroll"}}>
-                        
+                        {RenderOptions()}
                     </div>
                     <button className='ButtonHoverEffect SubmitButton' onClick={(e)=>{SendSelected()}}>Submit</button>
                 </div>
